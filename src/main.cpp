fa1142a54bb585fa7eb71a5241a933d137746873
Moon-794 <moon0795@outlook.com>
2024-05-06 21:50:54 +0100

    Connecting to localhost

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,33 @@
+#include <iostream>
+
+#include <cstring>
+
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <unistd.h>
+#include <errno.h>
+
 int main()
 {
+    struct addrinfo hints, *res;
+    int socketfd;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    getaddrinfo("127.0.0.0", "3500", &hints, &res);
+
+    socketfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    
+    if(connect(socketfd, res->ai_addr, res->ai_addrlen) == -1)
+    {
+        close(socketfd);
+        std::cout << res->ai_addr << "\n";
+        perror("client: connect");
+    }
+
     return 0;
 }
