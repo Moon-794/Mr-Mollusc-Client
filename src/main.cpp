d778597448eba7740f4ef5e0af1dbcb656d655d6
Moon-794 <moon0795@outlook.com>
2024-06-07 02:15:24 +0100

    SDL2 setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdio.h>
 
 #include <cstring>
 
@@ -9,7 +10,55 @@
 #include <unistd.h>
 #include <errno.h>
 
-int main()
+#include "SDL.h"
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+void ConnectToServer();
+
+int main(int argc, char* args[])
+{
+    SDL_Window* window = NULL;
+    SDL_Surface* surface = NULL;
+
+    if(SDL_Init(SDL_INIT_VIDEO) == -1)
+    {
+        //Error Occurred initialising SDL
+        printf("Error: Failed to Initialise SDL >> %s\n", SDL_GetError());
+        exit(-1);
+    }
+
+    printf("SDL Initialised...\n");
+
+    window = SDL_CreateWindow("Mr Mollusc", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+
+    if(window == NULL)
+    {
+        //Error occurred creating window
+        printf("Error: Failed to create window >> %s\n", SDL_GetError());
+
+        SDL_Quit();
+        exit(-1);
+    }
+
+    surface = SDL_GetWindowSurface(window);
+
+    if(surface == NULL)
+    {
+        //Error occurred getting window surface
+        printf("Error: Failed to get window surface >> %s\n", SDL_GetError());
+
+        SDL_Quit();
+        exit(-1);
+    }
+
+    SDL_UpdateWindowSurface(window);
+
+    SDL_Quit();
+}
+
+void ConnectToServer()
 {
     struct addrinfo hints, *res;
     int socketfd;
@@ -28,6 +77,4 @@ int main()
         std::cout << res->ai_addr << "\n";
         perror("client: connect");
     }
-
-    return 0;
 }
